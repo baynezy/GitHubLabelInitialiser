8feb88b60da687039610b9dfcd1feff5958872c7
Simon Baynes <baynezy@gmail.com>
2016-03-27 20:21:08 +0100

    Created skeleton for GitHubApi

---
+++ src/GitHubLabelInitialiser/GitHubApi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GitHubLabelInitialiser
+{
+	public class GitHubApi : IGitHubApi
+	{
+		public Task<IList<GitHubLabel>> GetAllForRepository(string repositoryName)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<GitHubLabel> DeleteLabel(GitHubLabel label)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ test/GitHubLabelInitialiser.Test/GitHubApiTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace GitHubLabelInitialiser.Test
+{
+	[TestFixture]
+	class GitHubApiTest
+	{
+		[Test]
+		public void GitHubApi_ImplementsIGitHubApi()
+		{
+			var api = CreateApi();
+			
+			Assert.That(api, Is.InstanceOf<IGitHubApi>());
+		}
+
+		private static IGitHubApi CreateApi()
+		{
+			return new GitHubApi();
+		}
+	}
+}
