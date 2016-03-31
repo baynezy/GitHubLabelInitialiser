06bb80e9d843daeedba4a79b7853856d8f0f0415
baynezy <baynezy@gmail.com>
2016-03-31 12:10:34 +0000

    skip ci - static site regeneration

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['addlabel',['AddLabel',['../class_git_hub_label_initialiser_1_1_git_hub_api.html#af4d8681decfbc80711d58181ced936cc',1,'GitHubLabelInitialiser.GitHubApi.AddLabel()'],['../interface_git_hub_label_initialiser_1_1_i_git_hub_api.html#ae63ea2f05df4328bc17f8b06fe7d5c1a',1,'GitHubLabelInitialiser.IGitHubApi.AddLabel()']]],
-  ['addlabelstorepository',['AddLabelsToRepository',['../interface_git_hub_label_initialiser_1_1_i_label_manager.html#a994698ac0f3318a405ea77e7f9320016',1,'GitHubLabelInitialiser.ILabelManager.AddLabelsToRepository()'],['../class_git_hub_label_initialiser_1_1_label_manager.html#a2b581a4178e3b94ac9c95c9bc2e5fe6f',1,'GitHubLabelInitialiser.LabelManager.AddLabelsToRepository()']]],
+  ['addlabelstorepository',['AddLabelsToRepository',['../interface_git_hub_label_initialiser_1_1_i_label_manager.html#ad9c63b3234404522e3e3f5a0bdb19ae5',1,'GitHubLabelInitialiser.ILabelManager.AddLabelsToRepository()'],['../class_git_hub_label_initialiser_1_1_label_manager.html#a923f52cf9c3c47248897a6566fca69c3',1,'GitHubLabelInitialiser.LabelManager.AddLabelsToRepository()']]],
   ['assemblyinfo_2ecs',['AssemblyInfo.cs',['../_assembly_info_8cs.html',1,'']]]
 ];
