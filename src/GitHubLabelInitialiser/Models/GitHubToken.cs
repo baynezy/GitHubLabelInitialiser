11ffc7ee9d1a1d19bb2f5bc62c9efa5c862207a6
Simon Baynes <baynezy@gmail.com>
2016-05-05 06:26:26 +0100

    Added support for token based access to API. Closes #24

---
+++ src/GitHubLabelInitialiser/Models/GitHubToken.cs
@@ -0,0 +1,14 @@
+﻿using Octokit;
+
+namespace GitHubLabelInitialiser.Models
+{
+	public class GitHubToken : IGitHubAccess
+	{
+		public Credentials Credentials
+		{
+			get { return new Credentials(Token); }
+		}
+
+		public string Token { get; set; }
+	}
+}
